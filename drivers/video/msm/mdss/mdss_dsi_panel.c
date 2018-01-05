40e556a3c5b9041408e96879c54b18bb22d24492
BlackMesa <brother12@hotmail.it>
2018-01-05 12:16:36 +0100

    mdss: add powersuspend hooks

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,6 +30,10 @@
 /*#define PANEL_CMD_DEBUG*/
 #endif
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 /* NT35596 panel specific status variables */
@@ -751,6 +755,9 @@ end:
 #endif
 
 	pr_info("%s:-\n", __func__);
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 	return 0;
 }
 
@@ -797,6 +804,9 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 end:
 	pr_info("%s:-\n", __func__);
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 	return 0;
 }
 
