63c3b00c217b88fb0b68350bbee0731fb7132659
Malpa73 <kmktoczek@gmail.com>
2019-01-13 17:26:19 +0100

    fix

--- output/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 24 17:31:32 KST 2018"
-#define LINUX_COMPILE_BY "oms"
-#define LINUX_COMPILE_HOST "oms-Samsung-Desktop-System"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
